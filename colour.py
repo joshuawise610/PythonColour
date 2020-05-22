0998b5646c75142366b532158afcfae221c251b2
Joshua Wise <scrunch077@gmail.com>
2020-05-22 17:26:30 +0100

    Update license year

--- colour.py
+++ colour.py
@@ -1,6 +1,6 @@
 """A wrapper for the colorama module."""
 """
-Copyright 2019 Joshua Wise
+Copyright 2019 - 2020 Joshua Wise
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
